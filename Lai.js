431ecec1f535129c0f7f15403cbd4cf8dc4460e3
Aaron <perf3ctodst@gmail.com>
2018-08-17 10:25:50 +0100

    Add files via upload

--- Lai.js
+++ Lai.js
@@ -1,9 +1,19 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 const mineflayer = require('mineflayer');
 const navigatePlugin = require('mineflayer-navigate')(mineflayer);
 var blockFinderPlugin = require('mineflayer-blockfinder')(mineflayer);
-const bot = mineflayer.createBot({host: "localhost",
-  port: 60814, 
-  username: 'LAI' });
+const bot = mineflayer.createBot({host: "us.shotbow.net",
+  port: 25565, 
+  username: 'wdas',
+  password: 'wdas',
+  verbose: true,
+  version: 1.12, });
 // install the plugin
 navigatePlugin(bot);
 bot.loadPlugin(blockFinderPlugin);
@@ -12,16 +22,18 @@ var status = "init"
 var savedBlock
 // optional configuration
 bot.navigate.blocksToAvoid[132] = true; // avoid tripwire
+bot.navigate.blocksToAvoid[9] = true; // avoid still water
+bot.navigate.blocksToAvoid[8] = true; //avoid flowing water
 bot.navigate.blocksToAvoid[59] = false; // ok to trample crops
 bot.navigate.on('pathFound', function (path) {
-  bot.chat("found path. I can get there in " + path.length + " moves.");
+  console.log("found path. I can get there in " + path.length + " moves.");
 });
 bot.navigate.on('cannotFind', function (closestPath) {
-  bot.chat("unable to find path. getting as close as possible");
+  console.log("unable to find path. getting as close as possible");
   bot.navigate.walk(closestPath);
 });
 bot.navigate.on('arrived', function () {
-  bot.chat("I have arrived");
+  console.log("I have arrived");
   if (status === "refill"){
     bot.lookAt(savedBlock)
     var glassbottle = getBottleB()
@@ -31,27 +43,100 @@ bot.navigate.on('arrived', function () {
   }
 });
 bot.navigate.on('interrupted', function() {
-  bot.chat("stopping");
+  console.log("stopping");
 });
-bot.on('chat', function(username, message) {
+
+
+
+rl.on('line', (input) => {
   // navigate to whoever talks
-  if (username === bot.username) return;
-  const target = bot.players[username].entity;
-  if (message === 'come') {
-    bot.navigate.to(target.position);
-  } else if (message === 'stop') {
+  if (input === 'spawn') {
+    bot.navigate.to(vec3({x:0, y:64, z:0}));
+  } else if (input === 'stop') {
     bot.navigate.stop();
   }
-  else if (message === 'fight') {
-    rofl=setInterval(showZeds,200)
+  else if (input === 'init') {
+    init()
   }
-  else if (message === 'items') {
+  else if (input === 'fight') {
+    rofl=setInterval(showZeds,200)
+  }else if (input === 'forth') {
+    bot.navigate.to(vec3({x:3655, y:64, z:-1205}));
+  }
+  else if (input === 'items') {
     sayItems()
   }
-  else if (message === 'hello') {
-    bot.chat("Hello world, I am a bot designed to do cool things!")
+  else if (input === 'hello') {
+    console.log("Hello world, I am a bot designed to do cool things!")
   }
 });
+
+bot.on('spawn', function(username, message) {
+  console.log(bot.entity.position)
+});
+
+bot.on('message', (message) => {
+  console.log(message.toAnsi())
+})
+
+
+bot.on('health',function(){
+  console.log("Food:"+bot.food+" Saturation:"+bot.foodSaturation+" Health:"+bot.health)
+  if (bot.food<16){
+    var food = getFood()
+    console.log(food)
+    try{
+      bot.equip(food,'hand')
+      bot.activateItem()
+    }
+    catch(err){
+      console.log("I have no food!")
+    }
+  }
+  sayItems()
+
+});
+
+bot.on('experience',function(){
+  if (bot.experience.level<5){
+    var waterbottle = getBottle()
+    try{
+    bot.equip(waterbottle,'hand')
+    bot.activateItem()
+    console.log("RefilledWater")
+  }
+    catch(err){console.log("No waterbottle")}
+  }
+
+  var glass = getBottleB()
+
+  if (glass){
+  bot.findBlock({
+    point: bot.entity.position,
+    matching: 9,
+    maxdistance: 16,
+    count: 1,
+  }, function(err, blocks){
+    if (err){
+      console.log("Error occured "+err)
+    }
+    if (blocks.length) {
+      console.log("Found a water source at "+blocks[0].position)
+      bot.navigate.to(blocks[0].position)
+      savedBlock=blocks[0].position
+      status="refill"
+    }
+    else{
+    }
+
+
+
+  }
+
+
+  )}
+
+})
 
 function showZeds () {
   try{
@@ -60,7 +145,7 @@ function showZeds () {
   var i=0
   var len=closeZedsId.length
   if (len>0){
-    bot.equip(bot.inventory.findInventoryItem(276),'hand')
+    bot.equip(bot.inventory.findInventoryItem(268),'hand')
   }
   for(;i<len;i++){
   console.log(closeZedsId[i].position)
@@ -109,62 +194,33 @@ function getBottleB(){
   catch(err){return 0}
 }
 
+function init(){
+  bot.activateItem()
+  setTimeout(function (){
+  const window=bot.currentWindow || bot.inventory
+  console.log("Passed first")
+  bot.clickWindow(2,0,0)
+  setTimeout(function (){
+    const window=bot.currentWindow || bot.inventory
+    console.log("Passed second")
+    bot.clickWindow(1,0,0)
+    setTimeout(function (){
+      const window=bot.currentWindow || bot.inventory
+      console.log("Passed third")
+      bot.clickWindow(0,0,0)
+      setTimeout(function (){
+        console.log(bot.physics)
+        console.log(bot.entity.position)
 
-bot.on('health',function(){
-  console.log("Food:"+bot.food+" Saturation:"+bot.foodSaturation+" Health:"+bot.health)
-  if (bot.food<16){
-    var food = getFood()
-    console.log(food)
-    try{
-      bot.equip(food,'hand')
-      bot.activateItem()
-    }
-    catch(err){
-      console.log("I have no food!")
-    }
-  }
-  sayItems()
-
-});
-
-bot.on('experience',function(){
-  if (bot.experience.level<5){
-    var waterbottle = getBottle()
-    try{
-    bot.equip(waterbottle,'hand')
-    bot.activateItem()
-    console.log("RefilledWater")
-  }
-    catch(err){console.log("No waterbottle")}
-  }
-
-  var glass = getBottleB()
-
-  if (glass){
-  bot.findBlock({
-    point: bot.entity.position,
-    matching: 9,
-    maxdistance: 16,
-    count: 1,
-  }, function(err, blocks){
-    if (err){
-      console.log("Error occured "+err)
-    }
-    if (blocks.length) {
-      bot.chat("Found a water source at "+blocks[0].position)
-      bot.navigate.to(blocks[0].position)
-      savedBlock=blocks[0].position
-      status="refill"
-    }
-    else{
-      bot.chat("No water source")
-    }
+      },1000)
 
 
+    },1000)
 
-  }
+
+  },1000)
+
+},1000)
 
 
-  )}
-
-})
+}
